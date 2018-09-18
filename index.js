8dec92a194d22055eaaf0cbb8e083607f29691c0
Robsonsjre <robsonsjre@gmail.com>
2018-09-18 13:59:06 +0800

    commited something

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log(req)
-  res.send({ hi: "there" });
+  res.send({ hi: "thereeee" });
 });
 
 app.get("/teste", (req, res) => {
   res.send({ teste: "oi" })
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT)
