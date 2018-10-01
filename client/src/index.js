5fef1d49fa15f94fdeabb31c80b64a5f71a927e4
Robsonsjre <robsonsjre@gmail.com>
2018-10-01 16:06:12 +0800

    added raw initial configuration to react/redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/App";
+
+// Create store:
+// 1) argument: Reducers
+// 2) not got yet, i think something to server side render
+// 3) Middlewares, redux thunk and saga enters here
+const store = createStore(() => [], {}, applyMiddleware());
+
+// Envolve the first top component with provider to give access to store from
+//all components and pass store as a props.
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root") //Id root at index.html
+);
