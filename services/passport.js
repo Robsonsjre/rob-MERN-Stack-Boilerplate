b22da16faf06f15cd0b58bd3b051066aa43f5251
Robsonsjre <robsonsjre@gmail.com>
2018-10-01 16:05:32 +0800

    refactor to async await

--- services/passport.js
+++ services/passport.js
@@ -11,11 +11,10 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  console.log('deserializeUser')
-  User.findById(id)
-    .then((user, err) => {
-      done(null, user)
-    });
+  console.log("deserializeUser");
+  User.findById(id).then((user, err) => {
+    done(null, user);
+  });
 });
 
 passport.use(
@@ -26,25 +25,20 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       const profileId = profile ? profile.id.toString() : false;
       const profileName = profile ? profile.displayName : "user" + profileId;
       if (profileId) {
-        User.findOne({ googleId: profileId }).then((existingUser, err) => {
-          console.log("existingUser", existingUser);
-          if (!existingUser) {
-            new User({ googleId: profileId, name: profileName })
-              .save()
-              .then(user => done(null, user));
-          } else {
-            done(null, existingUser);
-          }
-        });
+        const existingUser = await User.findOne({ googleId: profileId });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+        const user = await new User({ googleId: profileId, name: profileName}).save();
+        done(null, user);
       }
     }
   )
 );
-
 
 // passport.use(
 //   new FacebookStrategy(
