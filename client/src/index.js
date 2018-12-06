f675f37d46f197a678e5e363fb8563d8b5559dc7
Robsonsjre <robsonsjre@gmail.com>
2018-12-06 18:08:52 +0800

    added reduxDevTools

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,14 @@ import reduxThunk from 'redux-thunk'
 import axios from 'axios';
 import App from "./components/App";
 import reducers from "./reducers"
+import { composeWithDevTools } from "redux-devtools-extension";
 // Create store:
 // 1) argument: Reducers
 // 2) not got yet, i think something to server side render
 // 3) Middlewares, redux thunk and saga enters here
-const store = createStore(reducers, applyMiddleware(reduxThunk));
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(reduxThunk)
+));
 window.axios = axios;
 // Envolve the first top component with provider to give access to store from
 //all components and pass store as a props.
