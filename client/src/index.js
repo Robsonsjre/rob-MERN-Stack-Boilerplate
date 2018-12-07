ce4cd9a1e68d335dbc86d5b55f0fd70771e1788e
Robsonsjre <robsonsjre@gmail.com>
2018-12-07 14:44:24 +0800

    added router object to redux global state

--- client/src/index.js
+++ client/src/index.js
@@ -3,24 +3,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { routerMiddleware, connectRouter, ConnectedRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
 import reduxThunk from 'redux-thunk'
 import axios from 'axios';
 import App from "./components/App";
-import reducers from "./reducers"
+import { createRootReducer } from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 // Create store:
 // 1) argument: Reducers
 // 2) not got yet, i think something to server side render
 // 3) Middlewares, redux thunk and saga enters here
-const store = createStore(reducers, composeWithDevTools(
-  applyMiddleware(reduxThunk)
-));
+const history = createBrowserHistory();
+
+const initialState = {}
+const store = createStore(
+  createRootReducer(history),
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(routerMiddleware(history)),
+    applyMiddleware(reduxThunk)
+  )
+);
 window.axios = axios;
 // Envolve the first top component with provider to give access to store from
 //all components and pass store as a props.
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.querySelector("#root") //Id root at index.html
 );
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import { reducer as reduxForm } from 'redux-form';
+import { connectRouter } from "connected-react-router";
 import authReducer from "./authReducer";
 import surveyReducer from "./surveyReducer";
 
 //redux-form need to be assigned to the key 'form'
-export default combineReducers({
+export const createRootReducer = (history) => combineReducers({
   auth: authReducer,
   form: reduxForm,
-  surveys: surveyReducer
-})
+  surveys: surveyReducer,
+  router: connectRouter(history)
+});
